a49db4cf881d8ab1c2f7ef9d4a55966c5e1508e7
Your Name <you@example.com>
2016-01-24 01:46:58 +0300

    Added visits

---
+++ domain/src/main/java/ru/itsphere/subscription/domain/Visit.java
@@ -0,0 +1,53 @@
+package ru.itsphere.subscription.domain;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+
+/**
+ * Represents abstraction for subscription's visits
+ */
+@DatabaseTable
+public class Visit {
+    @DatabaseField(generatedId = true)
+    private long id;
+    @DatabaseField
+    private Date startDate;
+    @DatabaseField
+    private Date endDate;
+    @DatabaseField
+    private long subscriptionId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public long getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(long subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+}
